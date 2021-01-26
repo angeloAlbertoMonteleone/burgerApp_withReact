8c9f4a2f206e87924e85e7a8478a2caa773b48ed
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-01-26 21:08:21 +0100

    .reduce(), condictional statements added

--- src/components/Layout/Burger/Burger.js
+++ src/components/Layout/Burger/Burger.js
@@ -3,16 +3,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => { 
-    const transformedIngredients = Object.keys(props.ingredients).map(ing => {
-        return [...Array(props.ingredients[ing])].map((_, 1) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(ing => {
+        return [...Array(props.ingredients[ing])] .map((_, i) => {
            return <BurgerIngredient key={ing + i} type={ing} />
-        });
-        });
+        })
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if(transformedIngredients.length === 0) {
+            transformedIngredients = <p>You can start adding ingredients!</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
