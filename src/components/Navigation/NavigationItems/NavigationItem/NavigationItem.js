ef2105db850a6c3486ba29792b5086f39c1f426d
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-05 13:49:27 +0100

    first commit navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './NavigationItem.css'
+const navigationItem = () => (
+        <li className={classes.NavigationItem}>
+            <a href="/">A link</a>
+        </li>
+);
+
+export default navigationItem;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from 'NavigationItems.css';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        
+    </ul>
+)
+
+export default navigationItems;
