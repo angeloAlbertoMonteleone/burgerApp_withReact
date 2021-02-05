f6062f5207759a6027cf6e1ccf2f40a2e068d0c9
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-05 13:54:22 +0100

    Toolbar element, Content css added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Aux from '../../Auxiliary/Auxiliary';
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = (props) => {
     return (
         <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
+            <Toolbar></Toolbar>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
     </Aux>
     )
     
